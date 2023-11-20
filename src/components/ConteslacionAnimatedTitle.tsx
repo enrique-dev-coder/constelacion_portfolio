d91d5b34001ce739e685da61b47f4424023484d5
Enrique <enrique_alvarado95@hotmail.com>
2023-11-20 11:36:50 -0600

    added bg to the svg animated title

--- src/components/ConteslacionAnimatedTitle.tsx
+++ src/components/ConteslacionAnimatedTitle.tsx
@@ -18,27 +18,27 @@ import {
 } from "./ConstelacionLetters";
 const ConteslacionAnimatedTitle = () => {
   return (
-    <>
-      <HalfCircle1 className="ml-[140px]" />
+    <div className="z-10 relative">
+      <HalfCircle1 className="ml-[140px] z-10" />
       <div className="flex">
-        <C className="mr-[23px]" />
-        <O className="mr-[23px] mb-[50px] -translate-y-2" />
-        <N className="mr-[23px] mb-[20px]" />
-        <S className="mr-[23px]" />
-        <T className="mr-[23px] mt-[60px] " />
-        <E className="mr-[23px] mt-[80px] " />
-        <L className="mr-[23px] mt-[30px]" />
-        <A className="mr-[23px]" />
-        <C2 className="mr-[23px] mt-[30px]" />
-        <I className="mr-[23px] mt-[40px]" />
+        <C className="mr-[23px] z-10  " />
+        <O className="mr-[23px] mb-[50px] -translate-y-2 z-10 " />
+        <N className="mr-[23px] mb-[20px] z-10  " />
+        <S className="mr-[23px] z-10 " />
+        <T className="mr-[23px] mt-[60px] z-10  " />
+        <E className="mr-[23px] mt-[80px] z-10  " />
+        <L className="mr-[23px] mt-[30px] z-10 " />
+        <A className="mr-[23px] z-10 " />
+        <C2 className="mr-[23px] mt-[30px] z-10  " />
+        <I className="mr-[23px] mt-[40px] z-10  " />
         <div className="flex flex-col items-center">
-          <Acento className="mb-[10px]" />
-          <O2 className="mr-[23px] mb-[20px]" />
+          <Acento className="mb-[10px] z-10 " />
+          <O2 className="mr-[23px] mb-[20px] z-10 " />
         </div>
-        <N2 className="mr-[23px] mt-[20px]" />
+        <N2 className="mr-[23px] mt-[20px] z-10 " />
       </div>
-      <HalfCircle2 className="ml-[50px] -mt-[54px]" />
-    </>
+      <HalfCircle2 className="ml-[50px] -mt-[54px] z-10 " />
+    </div>
   );
 };
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ConteslacionAnimatedTitle from './ConteslacionAnimatedTitle';
-import ImageRow from './ImageRow';
-import ScrollInfo from './ScrollInfo';
+import React from "react";
+import ConteslacionAnimatedTitle from "./ConteslacionAnimatedTitle";
+import Circle from "../../public/assets/circle.svg";
+import ImageRow from "./ImageRow";
+import ScrollInfo from "./ScrollInfo";
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <section className=" w-screen h-screen  bg-purpleSoft flex flex-col items-center overflow-hidden  justify-center relative">
-      <div className=" z-10 bg-purpleSoft">
+      <div className=" z-10 relative ">
+        <Image alt="" src={Circle} className="bg-red absolute z-[5] -top-4" />
         <ConteslacionAnimatedTitle />
       </div>
       <ScrollInfo />
@@ -14,68 +17,68 @@ const Hero = () => {
       <div className="absolute top-0 z-0">
         <ImageRow
           images={[
-            'kidden',
-            'RTF',
-            'SOLE',
-            'TDW',
-            'TUCSA',
-            'kidden',
-            'RTF',
-            'TUCSA',
+            "kidden",
+            "RTF",
+            "SOLE",
+            "TDW",
+            "TUCSA",
+            "kidden",
+            "RTF",
+            "TUCSA",
           ]}
         />
         <ImageRow
           images={[
-            'TUCSA',
+            "TUCSA",
 
-            'TDW',
-            'kidden',
-            'SOLE',
-            'kidden',
-            'RTF',
+            "TDW",
+            "kidden",
+            "SOLE",
+            "kidden",
+            "RTF",
 
-            'TDW',
-            'TUCSA',
+            "TDW",
+            "TUCSA",
           ]}
         />
         <ImageRow
           images={[
-            'SOLE',
-            'TUCSA',
-            'TDW',
-            'kidden',
-            'RTF',
-            'TUCSA',
-            'RTF',
-            'TUCSA',
-            'SOLE',
+            "SOLE",
+            "TUCSA",
+            "TDW",
+            "kidden",
+            "RTF",
+            "TUCSA",
+            "RTF",
+            "TUCSA",
+            "SOLE",
           ]}
         />
         <ImageRow
           images={[
-            'kidden',
-            'RTF',
-            'SOLE',
-            'TDW',
-            'TUCSA',
-            'kidden',
-            'RTF',
-            'TUCSA',
-            'SOLE',
+            "kidden",
+            "RTF",
+            "SOLE",
+            "TDW",
+            "TUCSA",
+            "kidden",
+            "RTF",
+            "TUCSA",
+            "SOLE",
           ]}
         />
         <ImageRow
           images={[
-            'TUCSA',
+            "TUCSA",
 
-            'TDW',
-            'kidden',
-            'SOLE',
-            'kidden',
-            'RTF',
+            "TDW",
+            "kidden",
+            "SOLE",
+            "kidden",
+            "RTF",
 
-            'TDW',
-            'TUCSA',
+            "TDW",
+            "TUCSA",
           ]}
         />
       </div>
